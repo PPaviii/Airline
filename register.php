0c364a0bb644d9bfc4a3cb3521a9c3a3cab887b7
Francesco Pavan <pavanfrancesco87@gmail.com>
2019-06-04 17:10:38 +0200

    Changed the method from GET to POST
    Also added some error managing lines of code in the registerOk page

--- register.php
+++ register.php
@@ -8,11 +8,9 @@
 </head>
 <body>
 
-<?php session_start() ?>
-
 <h2>Sign Up</h2>
 
-<form id="register" action='registerOk.php' method='get' onsubmit="return check();">
+<form id="register" action='registerOk.php' method='post' onsubmit="return check();">
     <p>
     <label for="user">E-mail</label>
     <input type="email" name="user" id="name" placeholder="john@doe.us" style="text-align: center">
@@ -22,13 +20,6 @@
     <label for="pass">Password:&nbsp;&nbsp;</label>
     <input type="password" name="pass" id="pass" placeholder="********" style="text-align: center">
     </p><br>
-
-    <?php
-        $rand = rand();
-        $_SESSION['rand'] = $rand;
-
-        echo "<input type=\"hidden\" value=\"$rand\" name=\"randcheck\">";
-    ?>
 
     <button type="submit" name="submit">Sign Up</button><br><br>
 </form>
--- registerOk.php
+++ registerOk.php
@@ -2,8 +2,19 @@
 
 session_start();
 
-if (!isset($_POST["user"]) && !isset($_POST["pass"])) {
+if (isset($_POST["user"]) && isset($_POST["pass"])) {
+    $username = $_POST["user"];
+    $password = password_hash($_POST["pass"], PASSWORD_BCRYPT);
 
+    echo "<script type='text/javascript'>";
+    echo "window.alert('Registration completed successfully');";
+    echo "window.location = 'index.php';";
+    echo "</script>";
+
+}else{
+    echo "<h2>A problem has occurred</h2>";
+    echo "<p>No change to the database were done.</p>";
+    echo "<a href='register.php'>Return to the register page</a>";
 }
 
 ?>
