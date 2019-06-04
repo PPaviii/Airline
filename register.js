9d1cc9af3584bc8ae78eafa820eb4d3b03ac4606
Francesco Pavan <pavanfrancesco87@gmail.com>
2019-06-04 16:40:05 +0200

    Added complete registration form
    In this version the registration form works perfectly. I have added a php page to update the db (registerOk), but it's not complete

--- register.js
+++ register.js
@@ -1,26 +1,53 @@
 function check(){
 
-    var formParam = document.getElementById("register");
+    let formParam = document.getElementById("register");
 
-    var mail = formParam.elements[0].value;
-    var password = formParam.elements[1].value;
+    let mail = formParam.elements[0].value;
+    let password = formParam.elements[1].value;
 
-    resultEmail = validateEmail(mail);
-    resultPassword = validatePassword(password);
+    let resultEmail = validateEmail(mail);
+    let resultPassword = validatePassword(password);
 
+    if(resultEmail === false || resultPassword === false){
+        window.alert("Your username or password do not respect the registration policies. Try again");
+        document.getElementById("name").value = "";
+        document.getElementById("pass").value = "";
+        return false;
+    }else{
+        return true;
+    }
 }
 
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    return Boolean(re.test(String(email).toLowerCase()));
 }
 
 function validatePassword(password) {
 
-    for(var i = 0; i < strlen(password); i++){
-        if(password[i] === password[i].toLowerCase()){
+    if(password.length < 2){
+        return false;
+    }
 
+    for(let i = 0; i < password.length; i++){
+        if((password[i] >= 'a' && password[i] <= 'z') && isNaN(parseInt(password[i], 10))){
+            break;
+        }
+
+        if(i === password.length - 1){
+            return false;
         }
     }
 
+    for(let j = 0; j < password.length; j++){
+        if((password[j] >= 'A' && password[j] <= 'Z') || !isNaN(parseInt(password[j], 10))){
+            break;
+        }
+
+        if(j === password.length - 1){
+            return false;
+        }
+    }
+
+    return true;
 }
--- register.php
+++ register.php
@@ -8,9 +8,11 @@
 </head>
 <body>
 
+<?php session_start() ?>
+
 <h2>Sign Up</h2>
 
-<form id="register" action='' method='post'>
+<form id="register" action='registerOk.php' method='get' onsubmit="return check();">
     <p>
     <label for="user">E-mail</label>
     <input type="email" name="user" id="name" placeholder="john@doe.us" style="text-align: center">
@@ -18,10 +20,17 @@
 
     <p>
     <label for="pass">Password:&nbsp;&nbsp;</label>
-    <input type="password" name="pass" id="name" placeholder="********" style="text-align: center">
+    <input type="password" name="pass" id="pass" placeholder="********" style="text-align: center">
     </p><br>
 
-    <button type="submit" name="submit" onclick="check()">Sign Up</button><br><br>
+    <?php
+        $rand = rand();
+        $_SESSION['rand'] = $rand;
+
+        echo "<input type=\"hidden\" value=\"$rand\" name=\"randcheck\">";
+    ?>
+
+    <button type="submit" name="submit">Sign Up</button><br><br>
 </form>
 
 </body>
