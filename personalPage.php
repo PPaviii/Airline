4752e9262053f7ef1cfe5505e8a9f993e1b2067b
Francesco Pavan <pavanfrancesco87@gmail.com>
2019-06-06 11:43:14 +0200

    Changed personalPage name + fixed some forms

---
+++ personalPage.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Airline Personal Home Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<h2>Airline Personal Homepage</h2>
+
+<?php
+
+session_start();
+$_SESSION["error"] = 0; //flush previous error in the login form
+
+/*
+    status legend:
+        - 0: free
+        - 1: reserved
+        - 2: occupied
+*/
+
+$rows = 10; //places
+$columns = 6; //seats
+
+$servername = "localhost";
+$username = "s264970";
+$password = "chalingt";
+
+$conn = new mysqli($servername, $username, $password, "s264970");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "<table>";
+echo "<tr>";
+echo "<th></th>";
+
+for ($x = ord('A'); $x < ord('A') + $columns; $x++) {
+    echo "<th>" . chr($x) ."</th>";
+}
+
+echo "</tr>";
+for($i = 1; $i <= $rows; $i++){
+    echo"<tr>";
+    echo "<th>$i</th>";
+    for($j = 1; $j <= $columns; $j++){
+        echo "<td><a>Posto</a></td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+
+$free = "SELECT COUNT(*) AS Free FROM Seat WHERE Status = 0";
+$reserved = "SELECT COUNT(*) AS Reserved FROM Seat WHERE Status = 1";
+$occupied = "SELECT COUNT(*) AS Occ FROM Seat WHERE Status = 2";
+$total = $rows * $columns;
+
+$resFree = $conn->query($free);
+$resRes = $conn->query($reserved);
+$resOcc = $conn->query($occupied);
+
+$rowFree = $resFree->fetch_assoc();
+$rowRes = $resRes->fetch_assoc();
+$rowOcc = $resOcc->fetch_assoc();
+
+echo "<br><p>Number of available seats: " . $rowFree["Free"] . "</p>";
+echo "<br><p>Number of reserved seats: " . $rowRes["Reserved"] . "</p>";
+echo "<br><p>Number of occupied seats: " . $rowOcc["Occ"] . "</p>";
+echo "<br><p>Total number of seats: $total</p>";
+
+echo "<form action='index.php' method='post'>";
+echo "<br><p style='color: green'>Now you are logged in and you can purchase airplane seats.</p><br>";
+echo "<input type=\"hidden\" value=\"1\" name=\"lout\">";
+echo "<button type=\"submit\" name=\"logout\">Log Out</button><br>";
+echo "</form>";
+
+echo "<br><form action='personalPage.php' method='post'>";
+echo "<button type=\"submit\" name=\"update\">Update Seats</button><br>";
+echo "</form>";
+
+?>
+
+</body>
+</html>
+
